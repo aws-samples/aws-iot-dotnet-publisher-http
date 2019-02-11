8305cba409c9113a839260ba319520e7cf37261c
sundarnarasiman <narasund@amazon.com>
2019-02-11 11:53:58 +0530

    added dotnetcore folder

---
+++ dotnetcore/Thermostat.cs
@@ -0,0 +1,15 @@
+namespace dotnetcore
+{
+
+    class Thermostat
+    {
+        public int ThermostatID { get; set; }
+
+        public int SetPoint { get; set; }
+
+        public int CurrentTemperature { get; set; }
+
+       
+    }
+
+}
